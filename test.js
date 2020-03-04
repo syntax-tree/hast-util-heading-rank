c16984cd695b34474102a6af344cbbb27d6b52fa
Titus Wormer <tituswormer@gmail.com>
2020-03-04 12:08:48 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var test = require('tape')
+var h = require('hastscript')
+var rank = require('.')
+
+test('rank', function(t) {
+  t.equal(
+    rank({type: 'text', value: '!'}),
+    null,
+    'should return null for non-elements'
+  )
+
+  t.equal(rank(h('p', '!')), null, 'should return null for non-headings')
+
+  t.equal(rank(h('h5', '!')), 5, 'should return the rank of a heading')
+
+  t.end()
+})
