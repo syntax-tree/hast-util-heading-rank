0ddb18b9df707492aaa1ff833cc89a7e0fd830be
Titus Wormer <tituswormer@gmail.com>
2020-11-02 16:52:55 +0100

    Refactor to improve bundle size

--- index.js
+++ index.js
@@ -2,19 +2,13 @@
 
 module.exports = headingRank
 
-var lowercaseH = 104 // `h`
-var digit0 = 48 // `0`
-var digit6 = 54 // `6`
-
 // Rank of a heading: H1 -> 1, H2 -> 2, etc.
 function headingRank(node) {
-  var name = (
-    (node && node.type === 'element' && node.tagName) ||
-    ''
-  ).toLowerCase()
+  var name =
+    (node && node.type === 'element' && node.tagName.toLowerCase()) || ''
   var code =
-    name.length === 2 && name.charCodeAt(0) === lowercaseH
+    name.length === 2 && name.charCodeAt(0) === 104 /* `h` */
       ? name.charCodeAt(1)
       : 0
-  return code > digit0 && code <= digit6 ? code - digit0 : null
+  return code > 48 /* `0` */ && code < 55 /* `7` */ ? code - 48 /* `0` */ : null
 }
