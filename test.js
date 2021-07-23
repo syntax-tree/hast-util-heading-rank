d4adeba7f322fff7f6769287c8bc4229324862a2
Titus Wormer <tituswormer@gmail.com>
2021-07-23 09:44:18 +0200

    Add `strict` to `tsconfig.json`

--- test.js
+++ test.js
@@ -3,11 +3,10 @@ import {h} from 'hastscript'
 import {headingRank} from './index.js'
 
 test('headingRank', (t) => {
-  // @ts-ignore runtime.
+  // @ts-expect-error runtime.
   t.equal(headingRank(), null, 'should return null for non-nodes')
 
   t.equal(
-    // @ts-ignore runtime.
     headingRank({type: 'text', value: '!'}),
     null,
     'should return null for non-elements'
