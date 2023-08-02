a40a0062f10d45ba09afe6966e80b80884de6822
Titus Wormer <tituswormer@gmail.com>
2023-08-02 15:13:22 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -2,33 +2,28 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import {h} from 'hastscript'
 import {headingRank} from './index.js'
-import * as mod from './index.js'
 
-test('headingRank', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['headingRank'],
-    'should expose the public api'
-  )
+test('headingRank', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'headingRank'
+    ])
+  })
 
-  // @ts-expect-error runtime.
-  assert.equal(headingRank(), null, 'should return null for non-nodes')
+  await t.test('should return null for non-nodes', async function () {
+    // @ts-expect-error runtime.
+    assert.equal(headingRank(), null)
+  })
 
-  assert.equal(
-    headingRank({type: 'text', value: '!'}),
-    null,
-    'should return null for non-elements'
-  )
+  await t.test('should return null for non-elements', async function () {
+    assert.equal(headingRank({type: 'text', value: '!'}), null)
+  })
 
-  assert.equal(
-    headingRank(h('p', '!')),
-    null,
-    'should return null for non-headings'
-  )
+  await t.test('should return null for non-headings', async function () {
+    assert.equal(headingRank(h('p', '!')), null)
+  })
 
-  assert.equal(
-    headingRank(h('h5', '!')),
-    5,
-    'should return the rank of a heading'
-  )
+  await t.test('should return the rank of a heading', async function () {
+    assert.equal(headingRank(h('h5', '!')), 5)
+  })
 })
